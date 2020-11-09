6752e9b21afddef04087553244ba92e357eeacb9
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2020-11-09 20:16:32 +0200

    Update bme280.js

--- bme280.js
+++ bme280.js
@@ -112,9 +112,9 @@ class bme280Driver {
         return reject("You must first call bme280.init()");
       }
       resolve({
-        temperature_C: (Math.round(this.readTemperature() * 100) / 100).toFixed(2),
-        humidity: (Math.round(this.readHumidity() * 100) / 100).toFixed(2),
-        pressure_hPa: (Math.round((this.readPressure()/100) * 100) / 100).toFixed(2),
+        temperature_C: (parseFloat(Math.round(this.readTemperature() * 100) / 100).toFixed(2)),
+        humidity: (parseFloat(Math.round(this.readHumidity() * 100) / 100).toFixed(2)),
+        pressure_hPa: (parseFloat(Math.round((this.readPressure()/100) * 100) / 100).toFixed(2)),
       });
     });
   }
